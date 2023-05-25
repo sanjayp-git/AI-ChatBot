cf981ad8367bbbc8a88203a9a02162bdedd68c2c
Ronak Vadodariya <ronakvadodariya1909@gmail.com>
2023-05-25 12:17:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import jsonfile from 'jsonfile';
+import moment from 'moment';
+import simpleGit from 'simple-git';
+import random from 'random';
+
+// Use the 'random' module as needed in your code.
+
+
+const FILE_PATH = './data.json';
+
+const makeCommit = n => {
+    if (n === 0) return simpleGit().push();
+    const x = random.int(0, 54);
+    const y = random.int(0, 6);
+    const DATE = moment().subtract(1,'y').add(1,'d').add(x,'w').add(y,'d').format('YYYY-MM-DD');
+
+const data = {
+  date: DATE
+}
+console.log(DATE);
+jsonfile.writeFile(FILE_PATH, data, () => {
+simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}, makeCommit.bind(this, --n));
+});
+
+}
+
+makeCommit(500);
